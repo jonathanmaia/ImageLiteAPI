9ec43930d60d1ec7ae98b13c87dc1ac8d3f4684a
Jonathan Maia <jonathanmaia@gmail.com>
2024-09-17 08:09:29 -0300

    Primeiro commit

---
+++ src/test/java/br/com/eunati/imageliteapi/ImageliteapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eunati.imageliteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageliteapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
